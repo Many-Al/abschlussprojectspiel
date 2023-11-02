97e0c6cd426b9f4d31b9fb4efff9535a1bfcc88c
mahmoudalame <alamemany27@gmail.com>
2023-11-02 10:43:46 +0100

    Initial commit

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,60 @@
+class Game(private val team: Team, private val finalBoss: FinalBoss, private val bag: Bag) {
+    private var underBoss: Opponent? = null
+    private val userInput = UserInput()
+    fun start() {
+
+        println("Welcome to the Game!")
+        team.printHeroesHp()
+        finalBoss.printOpponentHp()
+        println("Let the battle begin!")
+
+        while (team.isAnyHeroAlive() && (finalBoss.isAlive() || (underBoss != null && underBoss!!.isAlive()))) {
+
+            team.heroes.forEach { hero ->
+                if (hero.isAlive()) {
+                    println("${hero.name}'s turn:")
+                    val actionTarget = if (underBoss != null && underBoss!!.isAlive()) {
+                        println("Choose a target:")
+                        println("1. Final Boss")
+                        println("2. Under Boss")
+                        val targetChoice = userInput.getInt("Enter your choice (1 or 2): ")
+                        userInput.consumeNewLine()
+                        if (targetChoice == 1) finalBoss else underBoss!!
+                    } else {
+                        finalBoss
+                    }
+                    hero.doSomeAction(actionTarget, bag, team)
+                }
+            }
+
+            if (finalBoss.isAlive()) {
+                finalBoss.doSomeAction(team)
+            }
+
+            // Check if the UnderBoss needs to be created
+            if (finalBoss.isUsedUnderBoss && underBoss == null) {
+                underBoss = UnderBoss("UnderBoss", 250)
+                println("The Final Boss created the UnderBoss!")
+                underBoss!!.printOpponentHp()
+            }
+
+            if (underBoss != null && underBoss!!.isAlive()) {
+                underBoss!!.doSomeAction(team)
+            }
+
+            println()
+            team.printHeroesHp()
+            finalBoss.printOpponentHp()
+            if (underBoss != null) {
+                underBoss!!.printOpponentHp()
+            }
+        }
+
+        if (team.isAnyHeroAlive()) {
+            println("Congratulations! You defeated Them.")
+        } else {
+            println("You were defeated by Them. Game Over!")
+        }
+
+    }
+}
